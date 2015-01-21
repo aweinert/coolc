973c8a221ee1cb655822076c9f0bacbec97b401b
Alexander Weinert <alexander.weinert@rwth-aachen.de>
2015-01-21 23:26:25 +0100

    Make Let immutable

--- src/main/java/net/alexweinert/coolc/program/ast/Let.java
+++ src/main/java/net/alexweinert/coolc/program/ast/Let.java
@@ -14,10 +14,10 @@ import net.alexweinert.coolc.program.symboltables.TreeConstants;
  * See <a href="TreeNode.html">TreeNode</a> for full documentation.
  */
 public class Let extends Expression {
-    protected AbstractSymbol identifier;
-    protected AbstractSymbol type_decl;
-    protected Expression init;
-    protected Expression body;
+    final protected AbstractSymbol identifier;
+    final protected AbstractSymbol type_decl;
+    final protected Expression init;
+    final protected Expression body;
 
     /**
      * Creates "let" AST node.
@@ -41,11 +41,6 @@ public class Let extends Expression {
         body = a4;
     }
 
-    public TreeNode copy() {
-        return new Let(lineNumber, copy_AbstractSymbol(identifier), copy_AbstractSymbol(type_decl),
-                (Expression) init.copy(), (Expression) body.copy());
-    }
-
     public void dump(PrintStream out, int n) {
         out.print(Utilities.pad(n) + "let\n");
         dump_AbstractSymbol(out, n + 2, identifier);
@@ -65,7 +60,7 @@ public class Let extends Expression {
     }
 
     @Override
-    protected AbstractSymbol inferType(Class_ enclosingClass, ClassTable classTable, FeatureTable featureTable) {
+    protected AbstractSymbol inferType(Class enclosingClass, ClassTable classTable, FeatureTable featureTable) {
         // Make sure that we do not try to bindlet
         if (this.identifier.equals(TreeConstants.self)) {
             String errorString = "'self' cannot be bound in a 'let' expression.";
