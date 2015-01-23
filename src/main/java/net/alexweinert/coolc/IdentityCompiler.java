feae2c2ca5807b347f79be069daa641a6d62e876
Alexander Weinert <alexander.weinert@rwth-aachen.de>
2015-01-23 15:44:27 +0100

    Implement IdentityCompiler for testing

--- src/main/java/net/alexweinert/coolc/IdentityCompiler.java
+++ src/main/java/net/alexweinert/coolc/IdentityCompiler.java
@@ -1,7 +1,27 @@
 package net.alexweinert.coolc;
 
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Reader;
+import java.io.Writer;
+
+import net.alexweinert.coolc.lexer.Lexer;
+import net.alexweinert.coolc.parser.Parser;
+import net.alexweinert.coolc.program.ast.Program;
+import net.alexweinert.coolc.program.ast.visitors.PrettyPrinter;
+
 public class IdentityCompiler {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        try {
+            final Reader reader = new FileReader(args[0]);
+            final Parser parser = new Parser(new Lexer(reader));
+            Program program = (Program) parser.parse().value;
+            reader.close();
+            System.out.println(PrettyPrinter.printAst(program));
+        } catch (Throwable e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
     }
 }
