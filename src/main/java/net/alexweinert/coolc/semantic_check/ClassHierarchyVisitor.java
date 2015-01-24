17b21715916427af16ec1e6c7843a5306e6406c0
Alexander Weinert <alexander.weinert@rwth-aachen.de>
2015-01-24 12:44:52 +0100

    Implement ClassHierarchyVisitor

---
+++ src/main/java/net/alexweinert/coolc/semantic_check/ClassHierarchyVisitor.java
@@ -0,0 +1,34 @@
+package net.alexweinert.coolc.semantic_check;
+
+import net.alexweinert.coolc.program.ast.Class;
+import net.alexweinert.coolc.program.ast.Program;
+import net.alexweinert.coolc.program.ast.TreeNode;
+import net.alexweinert.coolc.program.ast.visitors.ASTVisitor;
+import net.alexweinert.coolc.program.symboltables.AbstractTable;
+
+public class ClassHierarchyVisitor extends ASTVisitor {
+    final private ClassHierarchyBuilder builder = new ClassHierarchyBuilder();
+
+    public static ClassHierarchy buildHierarchy(TreeNode ast) {
+        final ClassHierarchyVisitor visitor = new ClassHierarchyVisitor();
+        ast.acceptVisitor(visitor);
+        return visitor.builder.buildHierarchy();
+    }
+
+    @Override
+    public void visitProgramPreorder(Program program) {
+        // Install predefined classes
+        this.builder.addInheritance(AbstractTable.stringtable.addString("IO"),
+                AbstractTable.stringtable.addString("Object"));
+        this.builder.addInheritance(AbstractTable.stringtable.addString("Int"),
+                AbstractTable.stringtable.addString("Object"));
+        this.builder.addInheritance(AbstractTable.stringtable.addString("String"),
+                AbstractTable.stringtable.addString("Object"));
+        this.builder.addInheritance(AbstractTable.stringtable.addString("Bool"),
+                AbstractTable.stringtable.addString("Object"));
+    }
+
+    public void visitClassPreorder(Class classNode) {
+        this.builder.addInheritance(classNode.getIdentifier(), classNode.getParent());
+    }
+}
