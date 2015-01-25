0c4bde8d133890fda9cd17681c76ab6755b4b3a0
Alexander Weinert <alexander.weinert@rwth-aachen.de>
2015-01-25 12:52:31 +0100

    Implement ClassInterface

---
+++ src/main/java/net/alexweinert/coolc/semantic_check/ClassInterface.java
@@ -0,0 +1,24 @@
+package net.alexweinert.coolc.semantic_check;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import net.alexweinert.coolc.program.symboltables.AbstractSymbol;
+
+public class ClassInterface {
+    final private AbstractSymbol classIdentifier;
+    final private List<MethodSignature> methods;
+
+    ClassInterface(AbstractSymbol classIdentifier, List<MethodSignature> methods) {
+        this.classIdentifier = classIdentifier;
+        this.methods = methods;
+    }
+
+    public AbstractSymbol getClassIdentifier() {
+        return classIdentifier;
+    }
+
+    public List<MethodSignature> getMethods() {
+        return new LinkedList<>(methods);
+    }
+}
