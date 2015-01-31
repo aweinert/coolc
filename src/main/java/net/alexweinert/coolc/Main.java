edf87ae5f8c9a2caef3ec7f50f81e0c26525e542
Alexander Weinert <alexander.weinert@rwth-aachen.de>
2015-01-31 10:19:55 +0100

    Implement proper Main-class

---
+++ src/main/java/net/alexweinert/coolc/Main.java
@@ -0,0 +1,33 @@
+package net.alexweinert.coolc;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.Reader;
+import java.io.Writer;
+
+import net.alexweinert.coolc.lexer.Lexer;
+import net.alexweinert.coolc.parser.Parser;
+import net.alexweinert.coolc.program.ast.Program;
+import net.alexweinert.coolc.program.ast.visitors.PrettyPrinter;
+import net.alexweinert.coolc.semantic_check.SemanticChecker;
+
+public class Main {
+    public static void main(String[] args) {
+        final Program program;
+        try {
+            final Reader reader = new FileReader(args[0]);
+            final Parser parser = new Parser(new Lexer(reader));
+            program = (Program) parser.parse().value;
+            reader.close();
+        } catch (Throwable e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+            System.exit(1);
+            // We need to explicitly return here since java does not recognize
+            // System.exit as aborting control flow
+            return;
+        }
+
+        SemanticChecker.checkSemantics(program, new Output());
+    }
+}
