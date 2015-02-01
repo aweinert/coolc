9bbe309f4799154896eecf573b9fa476b91fb9dc
Alexander Weinert <alexander.weinert@rwth-aachen.de>
2015-02-01 18:49:24 +0100

    Use ParserFactory in Main

--- src/main/java/net/alexweinert/coolc/Main.java
+++ src/main/java/net/alexweinert/coolc/Main.java
@@ -1,26 +1,16 @@
 package net.alexweinert.coolc;
 
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.Reader;
-import java.io.Writer;
-
-import net.alexweinert.coolc.lexer.Lexer;
 import net.alexweinert.coolc.parser.Parser;
+import net.alexweinert.coolc.parser.ParserFactory;
 import net.alexweinert.coolc.program.ast.Program;
-import net.alexweinert.coolc.program.ast.visitors.PrettyPrinter;
 import net.alexweinert.coolc.semantic_check.SemanticChecker;
 
 public class Main {
     public static void main(String[] args) {
         final Program program;
         try {
-            final Reader reader = new FileReader(args[0]);
-            final Lexer lexer = new Lexer(reader);
-            lexer.set_filename(args[0]);
-            final Parser parser = new Parser(lexer);
+            final Parser parser = ParserFactory.createParserForFile(args[0]);
             program = (Program) parser.parse().value;
-            reader.close();
         } catch (Throwable e) {
             System.out.println(e.getMessage());
             e.printStackTrace();
