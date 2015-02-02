b940640a228df7ce36c31e21134b0009850f2fbe
Alexander Weinert <alexander.weinert@rwth-aachen.de>
2015-02-02 09:34:00 +0100

    Actually compare expected and actual program in ParserTest

--- src/test/java/net/alexweinert/coolc/parser/ParserTest.java
+++ src/test/java/net/alexweinert/coolc/parser/ParserTest.java
@@ -1,9 +1,16 @@
 package net.alexweinert.coolc.parser;
 
+import java.awt.List;
 import java.net.URI;
 import java.nio.file.Paths;
+import java.util.Collections;
+import java.util.LinkedList;
 
+import net.alexweinert.coolc.program.ast.Class;
+import net.alexweinert.coolc.program.ast.Classes;
+import net.alexweinert.coolc.program.ast.Features;
 import net.alexweinert.coolc.program.ast.Program;
+import net.alexweinert.coolc.program.symboltables.AbstractTable;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -15,10 +22,13 @@ public class ParserTest {
         try {
             final URI uri = this.getClass().getResource("/parser/positive/class-a.cl").toURI();
             final String path = Paths.get(uri).toString();
-            System.err.println(path);
             final Parser parser = ParserFactory.createParserForFile(path);
-            final Program program = (Program) parser.parse().value;
-            Assert.assertNotNull(program);
+            final Program actualProgram = (Program) parser.parse().value;
+            final Program expectedProgram = new Program(path, 2, new Classes(path, 1,
+                    Collections.singletonList(new Class(path, 1, AbstractTable.stringtable.addString("A"),
+                            AbstractTable.stringtable.addString("Object"),
+                            new Features(path, 1, Collections.EMPTY_LIST)))));
+            Assert.assertEquals(expectedProgram, actualProgram);
         } catch (Throwable t) {
             Assert.fail(t.getMessage());
         }
