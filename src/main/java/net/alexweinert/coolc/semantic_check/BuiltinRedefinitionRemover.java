9da3ea7a2d4223a388ac5492f3735b8e4d75d665
Alexander Weinert <alexander.weinert@rwth-aachen.de>
2015-02-05 09:33:47 +0100

    Use more specific Symbols in BuiltinRedefinitionRemover

--- src/main/java/net/alexweinert/coolc/semantic_check/BuiltinRedefinitionRemover.java
+++ src/main/java/net/alexweinert/coolc/semantic_check/BuiltinRedefinitionRemover.java
@@ -2,8 +2,8 @@ package net.alexweinert.coolc.semantic_check;
 
 import net.alexweinert.coolc.program.ast.Class;
 import net.alexweinert.coolc.program.ast.Program;
-import net.alexweinert.coolc.program.symboltables.AbstractSymbol;
 import net.alexweinert.coolc.program.symboltables.AbstractTable;
+import net.alexweinert.coolc.program.symboltables.IdSymbol;
 
 class BuiltinRedefinitionRemover {
 
@@ -11,18 +11,16 @@ class BuiltinRedefinitionRemover {
      * Removes the redefinitions of builtin classes from the program, if they exist
      */
     public static Program removeBuiltinRedefinition(Program program, ISemanticErrorReporter out) {
-        final Program withoutObjectClass = removeBuiltinClass(program, AbstractTable.stringtable.addString("Object"),
-                out);
-        final Program withoutBoolClass = removeBuiltinClass(program, AbstractTable.stringtable.addString("Bool"), out);
-        final Program withoutIntClass = removeBuiltinClass(program, AbstractTable.stringtable.addString("Int"), out);
-        final Program withoutStringClass = removeBuiltinClass(program, AbstractTable.stringtable.addString("String"),
-                out);
-        final Program withoutIOClass = removeBuiltinClass(program, AbstractTable.stringtable.addString("IO"), out);
+        final Program withoutObjectClass = removeBuiltinClass(program, AbstractTable.idtable.addString("Object"), out);
+        final Program withoutBoolClass = removeBuiltinClass(program, AbstractTable.idtable.addString("Bool"), out);
+        final Program withoutIntClass = removeBuiltinClass(program, AbstractTable.idtable.addString("Int"), out);
+        final Program withoutStringClass = removeBuiltinClass(program, AbstractTable.idtable.addString("String"), out);
+        final Program withoutIOClass = removeBuiltinClass(program, AbstractTable.idtable.addString("IO"), out);
 
         return withoutIOClass;
     }
 
-    private static Program removeBuiltinClass(Program program, AbstractSymbol identifier, ISemanticErrorReporter out) {
+    private static Program removeBuiltinClass(Program program, IdSymbol identifier, ISemanticErrorReporter out) {
         final Class classNode = program.getClass(identifier);
         if (classNode != null) {
             out.reportRedefinitionOfBuiltInClass(identifier, classNode);
