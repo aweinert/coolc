9e562287fcfac1221faf40474c844c5203f3f209
Alexander Weinert <alexander.weinert@rwth-aachen.de>
2015-02-06 17:29:08 +0100

    Use proper IdTable in ParserTest

--- src/test/java/net/alexweinert/coolc/parser/ParserTest.java
+++ src/test/java/net/alexweinert/coolc/parser/ParserTest.java
@@ -1,16 +1,14 @@
 package net.alexweinert.coolc.parser;
 
-import java.awt.List;
 import java.net.URI;
 import java.nio.file.Paths;
 import java.util.Collections;
-import java.util.LinkedList;
 
 import net.alexweinert.coolc.program.ast.Class;
 import net.alexweinert.coolc.program.ast.Classes;
 import net.alexweinert.coolc.program.ast.Features;
 import net.alexweinert.coolc.program.ast.Program;
-import net.alexweinert.coolc.program.symboltables.AbstractTable;
+import net.alexweinert.coolc.program.symboltables.IdTable;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -24,10 +22,9 @@ public class ParserTest {
             final String path = Paths.get(uri).toString();
             final Parser parser = ParserFactory.createParserForFile(path);
             final Program actualProgram = (Program) parser.parse().value;
-            final Program expectedProgram = new Program(path, 2, new Classes(path, 1,
-                    Collections.singletonList(new Class(path, 1, AbstractTable.stringtable.addString("A"),
-                            AbstractTable.stringtable.addString("Object"),
-                            new Features(path, 1, Collections.EMPTY_LIST)))));
+            final Program expectedProgram = new Program(path, 1, new Classes(path, 1,
+                    Collections.singletonList(new Class(path, 1, IdTable.getInstance().addString("A"), IdTable
+                            .getInstance().addString("Object"), new Features(path, 1, Collections.EMPTY_LIST)))));
             Assert.assertEquals(expectedProgram, actualProgram);
         } catch (Throwable t) {
             Assert.fail(t.getMessage());
