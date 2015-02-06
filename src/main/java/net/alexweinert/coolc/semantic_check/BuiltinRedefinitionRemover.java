23a2634f512b0bbeb05e34443004a1478f1b33f7
Alexander Weinert <alexander.weinert@rwth-aachen.de>
2015-02-06 18:06:28 +0100

    Test BuiltinRedefinitionRemover

--- src/main/java/net/alexweinert/coolc/semantic_check/BuiltinRedefinitionRemover.java
+++ src/main/java/net/alexweinert/coolc/semantic_check/BuiltinRedefinitionRemover.java
@@ -12,10 +12,14 @@ class BuiltinRedefinitionRemover {
      */
     public static Program removeBuiltinRedefinition(Program program, ISemanticErrorReporter out) {
         final Program withoutObjectClass = removeBuiltinClass(program, IdTable.getInstance().addString("Object"), out);
-        final Program withoutBoolClass = removeBuiltinClass(program, IdTable.getInstance().addString("Bool"), out);
-        final Program withoutIntClass = removeBuiltinClass(program, IdTable.getInstance().addString("Int"), out);
-        final Program withoutStringClass = removeBuiltinClass(program, IdTable.getInstance().addString("String"), out);
-        final Program withoutIOClass = removeBuiltinClass(program, IdTable.getInstance().addString("IO"), out);
+        final Program withoutBoolClass = removeBuiltinClass(withoutObjectClass,
+                IdTable.getInstance().addString("Bool"), out);
+        final Program withoutIntClass = removeBuiltinClass(withoutBoolClass, IdTable.getInstance().addString("Int"),
+                out);
+        final Program withoutStringClass = removeBuiltinClass(withoutIntClass, IdTable.getInstance()
+                .addString("String"), out);
+        final Program withoutIOClass = removeBuiltinClass(withoutStringClass, IdTable.getInstance().addString("IO"),
+                out);
 
         return withoutIOClass;
     }
---
+++ src/test/java/net/alexweinert/coolc/semantic_check/BuiltinRedefinitionRemoverTest.java
@@ -0,0 +1,65 @@
+package net.alexweinert.coolc.semantic_check;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+import org.junit.Assert;
+import net.alexweinert.coolc.program.ast.Class;
+import net.alexweinert.coolc.program.ast.Classes;
+import net.alexweinert.coolc.program.ast.Feature;
+import net.alexweinert.coolc.program.ast.Features;
+import net.alexweinert.coolc.program.ast.Program;
+import net.alexweinert.coolc.program.symboltables.IdSymbol;
+import net.alexweinert.coolc.program.symboltables.IdTable;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+
+public class BuiltinRedefinitionRemoverTest {
+
+    @Test
+    public void testObjectRemoval() {
+        this.testBuiltinRemoval(IdTable.getInstance().addString("Object"));
+    }
+
+    @Test
+    public void testBoolRemoval() {
+        this.testBuiltinRemoval(IdTable.getInstance().addString("Bool"));
+    }
+
+    @Test
+    public void testIntRemoval() {
+        this.testBuiltinRemoval(IdTable.getInstance().addString("Int"));
+    }
+
+    @Test
+    public void testStringRemoval() {
+        this.testBuiltinRemoval(IdTable.getInstance().addString("String"));
+    }
+
+    @Test
+    public void testIORemoval() {
+        this.testBuiltinRemoval(IdTable.getInstance().addString("IO"));
+    }
+
+    private void testBuiltinRemoval(IdSymbol builtinId) {
+        final ISemanticErrorReporter err = Mockito.mock(ISemanticErrorReporter.class);
+
+        final IdSymbol objectSymbol = IdTable.getInstance().addString("Object");
+        final IdSymbol otherSymbol = IdTable.getInstance().addString("Other");
+        final Class intRedefinition = new Class("test.cl", 1, builtinId, objectSymbol, new Features("test.cl", 1,
+                Collections.<Feature> emptyList()));
+        final Class otherClass = new Class("test.cl", 1, otherSymbol, objectSymbol, new Features("test.cl", 1,
+                Collections.<Feature> emptyList()));
+
+        final Program testProgram = new Program("test.cl", 1, new Classes("test.cl", 1, Arrays.asList(intRedefinition,
+                otherClass)));
+
+        final Program resultProgram = BuiltinRedefinitionRemover.removeBuiltinRedefinition(testProgram, err);
+
+        Assert.assertNull(resultProgram.getClass(builtinId));
+        Assert.assertEquals(otherClass, resultProgram.getClass(otherSymbol));
+
+        Mockito.verify(err).reportRedefinitionOfBuiltInClass(builtinId, intRedefinition);
+    }
+}
