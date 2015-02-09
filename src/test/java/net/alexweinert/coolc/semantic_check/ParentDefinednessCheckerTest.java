a5308456a5649c91167522cda615d84bc95cd93e
Alexander Weinert <alexander.weinert@rwth-aachen.de>
2015-02-09 23:15:18 +0100

    Test ParentDefinednessChecker

---
+++ src/test/java/net/alexweinert/coolc/semantic_check/ParentDefinednessCheckerTest.java
@@ -0,0 +1,31 @@
+package net.alexweinert.coolc.semantic_check;
+
+import net.alexweinert.coolc.program.ast.ASTFactory;
+import net.alexweinert.coolc.program.ast.Class;
+import net.alexweinert.coolc.program.ast.Program;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+public class ParentDefinednessCheckerTest {
+
+    @Test
+    public void testCheckParentDefinedness() {
+        final ASTFactory factory = new ASTFactory();
+        final Class undefParentClass = factory.classNode("ClassOne", "UndefClass");
+        final Class defParentClass = factory.classNode("ClassTwo", "Object");
+
+        final Program testProgram = factory.program(undefParentClass, defParentClass);
+
+        final ISemanticErrorReporter err = Mockito.mock(ISemanticErrorReporter.class);
+
+        final Program expectedProgram = factory.program(factory.classNode("ClassOne", "Object"), defParentClass);
+        final Program receivedProgram = ParentDefinednessChecker.checkParentDefinedness(testProgram, err);
+
+        Mockito.verify(err).reportUndefinedParentClass(undefParentClass);
+        Mockito.verifyNoMoreInteractions(err);
+
+        Assert.assertEquals(expectedProgram, receivedProgram);
+    }
+}
