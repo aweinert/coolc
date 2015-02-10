b99e4f901cd84809d4755f941a1f94fb6c7c7b19
Alexander Weinert <alexander.weinert@rwth-aachen.de>
2015-02-10 21:05:19 +0100

    Include stub of MultipleClassesRemover

---
+++ src/main/java/net/alexweinert/coolc/semantic_check/MultipleClassesRemover.java
@@ -0,0 +1,15 @@
+package net.alexweinert.coolc.semantic_check;
+
+import net.alexweinert.coolc.program.ast.Program;
+
+class MultipleClassesRemover {
+
+    /**
+     * Checks for multiple definitions of classes with the same identifier and removes all but one definition of these
+     */
+    public static Program removeMultipleClassDefinitions(Program program, SemanticErrorReporter error) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
--- src/main/java/net/alexweinert/coolc/semantic_check/SemanticChecker.java
+++ src/main/java/net/alexweinert/coolc/semantic_check/SemanticChecker.java
@@ -7,6 +7,7 @@ public class SemanticChecker {
     public static Program checkSemantics(Program program, Output out) {
         final SemanticErrorReporter error = new SemanticErrorReporter(out);
 
+        program = MultipleClassesRemover.removeMultipleClassDefinitions(program, error);
         program = BuiltinRedefinitionRemover.removeBuiltinRedefinition(program, error);
         program = BuiltinInheritanceChecker.checkBuiltinInheritance(program, error);
         program = ParentDefinednessChecker.checkParentDefinedness(program, error);
