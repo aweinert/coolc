34d0f109cef04d076e2b90c9b211098a41370a19
Alexander Weinert <alexander.weinert@rwth-aachen.de>
2015-02-12 17:52:42 +0100

    Implement ASTFactory#method and ASTFactory#formal

--- src/main/java/net/alexweinert/coolc/program/ast/ASTFactory.java
+++ src/main/java/net/alexweinert/coolc/program/ast/ASTFactory.java
@@ -1,6 +1,8 @@
 package net.alexweinert.coolc.program.ast;
 
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 
 import net.alexweinert.coolc.program.symboltables.IdSymbol;
 import net.alexweinert.coolc.program.symboltables.IdTable;
@@ -37,5 +39,26 @@ public class ASTFactory {
         final IdSymbol typeSymbol = IdTable.getInstance().addString(type);
 
         return new Attribute(this.filename, 1, identifierSymbol, typeSymbol, initializer);
+    }
+
+    public Method method(String identifier, String returnType, Formal... parameterTypes) {
+        final Expression noExpression = new NoExpression(this.filename, 1);
+        return this.method(identifier, returnType, noExpression, parameterTypes);
+    }
+
+    public Method method(String identifier, String returnType, Expression body, Formal... formalList) {
+        final IdSymbol identifierSymbol = IdTable.getInstance().addString(identifier);
+        final IdSymbol returnTypeSymbol = IdTable.getInstance().addString(returnType);
+
+        final Formals formals = new Formals(this.filename, 1, Arrays.asList(formalList));
+
+        return new Method(this.filename, 1, identifierSymbol, formals, returnTypeSymbol, body);
+    }
+
+    public Formal formal(String identifier, String type) {
+        final IdSymbol identifierSymbol = IdTable.getInstance().addString(identifier);
+        final IdSymbol typeSymbol = IdTable.getInstance().addString(type);
+
+        return new Formal(this.filename, 1, identifierSymbol, typeSymbol);
     }
 }
