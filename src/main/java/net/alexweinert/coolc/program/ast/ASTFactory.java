ac7aac413648d2a049d8d0d1ccf8ab66e7db9d4f
Alexander Weinert <alexander.weinert@rwth-aachen.de>
2015-02-13 12:16:51 +0100

    Make lineNumber modifiable in ASTFactory

--- src/main/java/net/alexweinert/coolc/program/ast/ASTFactory.java
+++ src/main/java/net/alexweinert/coolc/program/ast/ASTFactory.java
@@ -9,28 +9,35 @@ import net.alexweinert.coolc.program.symboltables.IdTable;
 
 public class ASTFactory {
     private final String filename;
+    private int lineNumber;
 
     public ASTFactory() {
-        this("");
+        this("", 1);
     }
 
-    public ASTFactory(String filename) {
+    public ASTFactory(String filename, int lineNumber) {
         this.filename = filename;
+        this.lineNumber = lineNumber;
+    }
+
+    public void setLineNumber(int lineNumber) {
+        this.lineNumber = lineNumber;
     }
 
     public Program program(Class... classes) {
-        return new Program(this.filename, 1, new Classes(this.filename, 1, Arrays.asList(classes)));
+        return new Program(this.filename, this.lineNumber, new Classes(this.filename, this.lineNumber,
+                Arrays.asList(classes)));
     }
 
     public Class classNode(String identifier, String parent, Feature... features) {
         final IdSymbol identifierSymbol = IdTable.getInstance().addString(identifier);
         final IdSymbol parentSymbol = IdTable.getInstance().addString(parent);
-        return new Class(this.filename, 1, identifierSymbol, parentSymbol, new Features(this.filename, 1,
-                Arrays.asList(features)));
+        return new Class(this.filename, this.lineNumber, identifierSymbol, parentSymbol, new Features(this.filename,
+                this.lineNumber, Arrays.asList(features)));
     }
 
     public Attribute attribute(String identifier, String type) {
-        final Expression noExpression = new NoExpression(this.filename, 1);
+        final Expression noExpression = new NoExpression(this.filename, this.lineNumber);
         return this.attribute(identifier, type, noExpression);
     }
 
@@ -38,11 +45,11 @@ public class ASTFactory {
         final IdSymbol identifierSymbol = IdTable.getInstance().addString(identifier);
         final IdSymbol typeSymbol = IdTable.getInstance().addString(type);
 
-        return new Attribute(this.filename, 1, identifierSymbol, typeSymbol, initializer);
+        return new Attribute(this.filename, this.lineNumber, identifierSymbol, typeSymbol, initializer);
     }
 
     public Method method(String identifier, String returnType, Formal... parameterTypes) {
-        final Expression noExpression = new NoExpression(this.filename, 1);
+        final Expression noExpression = new NoExpression(this.filename, this.lineNumber);
         return this.method(identifier, returnType, noExpression, parameterTypes);
     }
 
@@ -50,15 +57,15 @@ public class ASTFactory {
         final IdSymbol identifierSymbol = IdTable.getInstance().addString(identifier);
         final IdSymbol returnTypeSymbol = IdTable.getInstance().addString(returnType);
 
-        final Formals formals = new Formals(this.filename, 1, Arrays.asList(formalList));
+        final Formals formals = new Formals(this.filename, this.lineNumber, Arrays.asList(formalList));
 
-        return new Method(this.filename, 1, identifierSymbol, formals, returnTypeSymbol, body);
+        return new Method(this.filename, this.lineNumber, identifierSymbol, formals, returnTypeSymbol, body);
     }
 
     public Formal formal(String identifier, String type) {
         final IdSymbol identifierSymbol = IdTable.getInstance().addString(identifier);
         final IdSymbol typeSymbol = IdTable.getInstance().addString(type);
 
-        return new Formal(this.filename, 1, identifierSymbol, typeSymbol);
+        return new Formal(this.filename, this.lineNumber, identifierSymbol, typeSymbol);
     }
 }
