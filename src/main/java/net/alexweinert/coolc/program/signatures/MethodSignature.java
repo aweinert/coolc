22b9a6356124e24537f3582f2496cade10c449d2
Alexander Weinert <alexander.weinert@rwth-aachen.de>
2015-02-17 00:02:42 +0100

    Move MethodSignature to program.signatures

---
+++ src/main/java/net/alexweinert/coolc/program/signatures/MethodSignature.java
@@ -0,0 +1,40 @@
+package net.alexweinert.coolc.program.signatures;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import net.alexweinert.coolc.program.ast.Formal;
+import net.alexweinert.coolc.program.ast.Method;
+import net.alexweinert.coolc.program.symboltables.IdSymbol;
+
+public class MethodSignature {
+    private IdSymbol returnType;
+    private IdSymbol methodIdentifier;
+    private List<IdSymbol> argumentTypes;
+
+    public MethodSignature create(Method method) {
+        final List<IdSymbol> argumentTypes = new LinkedList<>();
+        for (Formal formal : method.getFormals()) {
+            argumentTypes.add(formal.getDeclaredType());
+        }
+        return new MethodSignature(method.getReturnType(), method.getName(), argumentTypes);
+    }
+
+    MethodSignature(IdSymbol returnType, IdSymbol methodIdentifier, List<IdSymbol> argumentTypes) {
+        this.returnType = returnType;
+        this.methodIdentifier = methodIdentifier;
+        this.argumentTypes = new LinkedList<>(argumentTypes);
+    }
+
+    public IdSymbol getReturnType() {
+        return returnType;
+    }
+
+    public IdSymbol getIdentifier() {
+        return this.methodIdentifier;
+    }
+
+    public List<IdSymbol> getArgumentTypes() {
+        return new LinkedList<>(argumentTypes);
+    }
+}
