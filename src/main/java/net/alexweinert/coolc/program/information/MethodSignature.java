f1a0395a604ccfcbd124a3eef5ee80e5eca658c3
Alexander Weinert <alexander.weinert@rwth-aachen.de>
2015-02-19 21:51:15 +0100

    Generate MethodSignature#hashCode and MethodSignature#equals

--- src/main/java/net/alexweinert/coolc/program/information/MethodSignature.java
+++ src/main/java/net/alexweinert/coolc/program/information/MethodSignature.java
@@ -37,4 +37,50 @@ public class MethodSignature {
     public List<IdSymbol> getArgumentTypes() {
         return new LinkedList<>(argumentTypes);
     }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((argumentTypes == null) ? 0 : argumentTypes.hashCode());
+        result = prime * result + ((methodIdentifier == null) ? 0 : methodIdentifier.hashCode());
+        result = prime * result + ((returnType == null) ? 0 : returnType.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        MethodSignature other = (MethodSignature) obj;
+        if (argumentTypes == null) {
+            if (other.argumentTypes != null) {
+                return false;
+            }
+        } else if (!argumentTypes.equals(other.argumentTypes)) {
+            return false;
+        }
+        if (methodIdentifier == null) {
+            if (other.methodIdentifier != null) {
+                return false;
+            }
+        } else if (!methodIdentifier.equals(other.methodIdentifier)) {
+            return false;
+        }
+        if (returnType == null) {
+            if (other.returnType != null) {
+                return false;
+            }
+        } else if (!returnType.equals(other.returnType)) {
+            return false;
+        }
+        return true;
+    }
 }
