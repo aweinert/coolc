d91f992646de7d93b062f9c8906cae98b6703454
Alexander Weinert <alexander.weinert@rwth-aachen.de>
2015-02-19 21:48:00 +0100

    Implement ClassSignature#getAttribute(s), ClassSignature#getMethodSig...

--- src/main/java/net/alexweinert/coolc/program/information/ClassSignature.java
+++ src/main/java/net/alexweinert/coolc/program/information/ClassSignature.java
@@ -1,12 +1,13 @@
 package net.alexweinert.coolc.program.information;
 
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.Map;
 
 import net.alexweinert.coolc.program.ast.Attribute;
 import net.alexweinert.coolc.program.symboltables.IdSymbol;
 
 class ClassSignature {
-
     private final Map<IdSymbol, Attribute> attributes;
     private final Map<IdSymbol, MethodSignature> methods;
 
@@ -15,4 +16,19 @@ class ClassSignature {
         this.methods = methods;
     }
 
+    public Collection<Attribute> getAttributes() {
+        return new HashSet<>(this.attributes.values());
+    }
+
+    public Collection<MethodSignature> getMethodSignatures() {
+        return new HashSet<>(this.methods.values());
+    }
+
+    public Attribute getAttribute(IdSymbol id) {
+        return this.attributes.get(id);
+    }
+
+    public MethodSignature getMethodSignature(IdSymbol id) {
+        return this.methods.get(id);
+    }
 }
