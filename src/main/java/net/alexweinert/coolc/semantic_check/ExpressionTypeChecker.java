7ed573e72d6f1fc697eb9bf5b3dcb475327633e0
Alexander Weinert <alexander.weinert@rwth-aachen.de>
2015-02-20 11:42:03 +0100

    Implement checking of arithmetic expressions

--- src/main/java/net/alexweinert/coolc/semantic_check/ExpressionTypeChecker.java
+++ src/main/java/net/alexweinert/coolc/semantic_check/ExpressionTypeChecker.java
@@ -1,12 +1,25 @@
 package net.alexweinert.coolc.semantic_check;
 
-import java.util.LinkedList;
 import java.util.Map;
 import java.util.Stack;
 
+import net.alexweinert.coolc.program.ast.Addition;
+import net.alexweinert.coolc.program.ast.ArithmeticNegation;
+import net.alexweinert.coolc.program.ast.BoolConst;
+import net.alexweinert.coolc.program.ast.Division;
+import net.alexweinert.coolc.program.ast.Expression;
+import net.alexweinert.coolc.program.ast.IntConst;
+import net.alexweinert.coolc.program.ast.LessThan;
+import net.alexweinert.coolc.program.ast.LessThanOrEquals;
+import net.alexweinert.coolc.program.ast.Multiplication;
+import net.alexweinert.coolc.program.ast.ObjectReference;
+import net.alexweinert.coolc.program.ast.StringConst;
+import net.alexweinert.coolc.program.ast.Subtraction;
 import net.alexweinert.coolc.program.ast.visitors.ASTVisitor;
+import net.alexweinert.coolc.program.information.ClassHierarchy;
 import net.alexweinert.coolc.program.information.DefinedClassSignature;
 import net.alexweinert.coolc.program.symboltables.IdSymbol;
+import net.alexweinert.coolc.program.symboltables.IdTable;
 
 class ExpressionTypeChecker extends ASTVisitor {
 
@@ -14,14 +27,16 @@ class ExpressionTypeChecker extends ASTVisitor {
 
     final private IdSymbol classId;
     final private Stack<VariablesScope> variablesScopes;
+    final private ClassHierarchy hierarchy;
     final private Map<IdSymbol, DefinedClassSignature> definedSignatures;
     final private SemanticErrorReporter err;
 
-    public ExpressionTypeChecker(IdSymbol classId, VariablesScope initialScope,
+    public ExpressionTypeChecker(IdSymbol classId, VariablesScope initialScope, ClassHierarchy hierarchy,
             Map<IdSymbol, DefinedClassSignature> definedSignatures, SemanticErrorReporter err) {
         this.classId = classId;
         this.variablesScopes = new Stack<>();
         this.variablesScopes.add(initialScope);
+        this.hierarchy = hierarchy;
         this.definedSignatures = definedSignatures;
         this.err = err;
     }
@@ -30,4 +45,99 @@ class ExpressionTypeChecker extends ASTVisitor {
         assert this.argumentTypes.size() == 1;
         return this.argumentTypes.peek();
     }
+
+    @Override
+    public void visitBoolConst(BoolConst boolConst) {
+        this.argumentTypes.push(ExpressionType.create(IdTable.getInstance().getBoolSymbol()));
+    }
+
+    @Override
+    public void visitIntConst(IntConst intConst) {
+        this.argumentTypes.push(ExpressionType.create(IdTable.getInstance().getIntSymbol()));
+    }
+
+    @Override
+    public void visitStringConstant(StringConst stringConst) {
+        this.argumentTypes.push(ExpressionType.create(IdTable.getInstance().getStringSymbol()));
+    }
+
+    @Override
+    public void visitObjectReference(ObjectReference objectReference) {
+        this.argumentTypes.push(this.variablesScopes.peek().getVariableType(objectReference.getVariableIdentifier()));
+    }
+
+    @Override
+    public void visitAdditionPostorder(Addition addition) {
+        this.visitBinaryArithmeticOperation(addition);
+    }
+
+    @Override
+    public void visitDivisionPostorder(Division division) {
+        this.visitBinaryArithmeticOperation(division);
+    }
+
+    @Override
+    public void visitMultiplicationPostorder(Multiplication multiplication) {
+        this.visitBinaryArithmeticOperation(multiplication);
+    }
+
+    @Override
+    public void visitSubtractionPostorder(Subtraction subtraction) {
+        this.visitBinaryArithmeticOperation(subtraction);
+    }
+
+    @Override
+    public void visitArithmeticNegationPostOrder(ArithmeticNegation arithmeticNegation) {
+        final IdSymbol intSymbol = IdTable.getInstance().getIntSymbol();
+
+        final IdSymbol argType = this.argumentTypes.pop().getTypeId(classId);
+        if (!this.hierarchy.conformsTo(argType, intSymbol)) {
+            err.reportTypeMismatch(arithmeticNegation, argType, intSymbol);
+        }
+
+        this.argumentTypes.push(ExpressionType.create(intSymbol));
+    }
+
+    private void visitBinaryArithmeticOperation(Expression operation) {
+        final IdSymbol intSymbol = IdTable.getInstance().getIntSymbol();
+
+        final IdSymbol rhsType = this.argumentTypes.pop().getTypeId(classId);
+        if (!this.hierarchy.conformsTo(rhsType, intSymbol)) {
+            err.reportTypeMismatch(operation, rhsType, intSymbol);
+        }
+
+        final IdSymbol lhsType = this.argumentTypes.pop().getTypeId(classId);
+        if (!this.hierarchy.conformsTo(lhsType, intSymbol)) {
+            err.reportTypeMismatch(operation, lhsType, intSymbol);
+        }
+
+        this.argumentTypes.push(ExpressionType.create(intSymbol));
+    }
+
+    @Override
+    public void visitLessThanPostorder(LessThan lessThan) {
+        this.visitArithmeticComparison(lessThan);
+    }
+
+    @Override
+    public void visitLessThanOrEqualsPostorder(LessThanOrEquals lessThanOrEquals) {
+        this.visitArithmeticComparison(lessThanOrEquals);
+    }
+
+    private void visitArithmeticComparison(Expression operation) {
+        final IdSymbol intSymbol = IdTable.getInstance().getIntSymbol();
+
+        final IdSymbol rhsType = this.argumentTypes.pop().getTypeId(classId);
+        if (!this.hierarchy.conformsTo(rhsType, intSymbol)) {
+            err.reportTypeMismatch(operation, rhsType, intSymbol);
+        }
+
+        final IdSymbol lhsType = this.argumentTypes.pop().getTypeId(classId);
+        if (!this.hierarchy.conformsTo(lhsType, intSymbol)) {
+            err.reportTypeMismatch(operation, lhsType, intSymbol);
+        }
+
+        this.argumentTypes.push(ExpressionType.create(IdTable.getInstance().getBoolSymbol()));
+    }
+
 }
