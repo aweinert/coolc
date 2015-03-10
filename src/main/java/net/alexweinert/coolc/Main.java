3137a110a37c97164054b100fbfd9bfca47e9252
Alexander Weinert <alexander.weinert@rwth-aachen.de>
2015-03-10 19:50:03 +0100

    Use typechecking in default compiler

--- src/main/java/net/alexweinert/coolc/Main.java
+++ src/main/java/net/alexweinert/coolc/Main.java
@@ -2,14 +2,15 @@ package net.alexweinert.coolc;
 
 import net.alexweinert.coolc.infrastructure.Compiler;
 import net.alexweinert.coolc.processors.cool.frontend.CoolParser;
+import net.alexweinert.coolc.processors.cool.typechecking.CoolTypeChecker;
 import net.alexweinert.coolc.processors.cool.unparser.CoolUnparser;
 import net.alexweinert.coolc.processors.io.fileopener.FileOpener;
 import net.alexweinert.coolc.processors.io.stringdumper.StringDumper;
 
 public class Main {
     public static void main(String[] args) {
-        final Compiler<String> compiler = new FileOpener(args[0]).append(new CoolParser()).append(new CoolUnparser())
-                .append(new StringDumper());
+        final Compiler<String> compiler = new FileOpener(args[0]).append(new CoolParser())
+                .append(new CoolTypeChecker(new Output())).append(new CoolUnparser()).append(new StringDumper());
         compiler.compile();
     }
 }
