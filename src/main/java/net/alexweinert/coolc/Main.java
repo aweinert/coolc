2ea4fb0daea7bcc0efcd6d3c47512138d2c61490
Alexander Weinert <alexander.weinert@rwth-aachen.de>
2015-03-11 22:43:49 +0100

    Use ProcessorBuilder in Main

--- src/main/java/net/alexweinert/coolc/Main.java
+++ src/main/java/net/alexweinert/coolc/Main.java
@@ -1,16 +1,12 @@
 package net.alexweinert.coolc;
 
 import net.alexweinert.coolc.infrastructure.Compiler;
-import net.alexweinert.coolc.processors.cool.frontend.CoolParser;
-import net.alexweinert.coolc.processors.cool.typechecking.CoolTypeChecker;
-import net.alexweinert.coolc.processors.cool.unparser.CoolUnparser;
-import net.alexweinert.coolc.processors.io.fileopener.FileOpener;
-import net.alexweinert.coolc.processors.io.stringdumper.StringDumper;
+import net.alexweinert.coolc.processors.ProcessorBuilder;
 
 public class Main {
     public static void main(String[] args) {
-        final Compiler<String> compiler = new FileOpener(args[0]).append(new CoolParser())
-                .append(new CoolTypeChecker(new Output())).append(new CoolUnparser()).append(new StringDumper());
+        final Compiler<String> compiler = new ProcessorBuilder().openFile(args[0]).parseCool().hierarchyCheck()
+                .typeCheck().dumpToConsole();
         compiler.compile();
     }
 }
