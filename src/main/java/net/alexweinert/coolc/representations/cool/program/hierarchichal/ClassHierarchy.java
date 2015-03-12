72ead23464af2bdd67eb265163b51f12986f27fa
Alexander Weinert <alexander.weinert@rwth-aachen.de>
2015-03-12 14:23:48 +0100

    Move Builder and Factory into ClassHierarchy

--- src/main/java/net/alexweinert/coolc/representations/cool/program/hierarchichal/ClassHierarchy.java
+++ src/main/java/net/alexweinert/coolc/representations/cool/program/hierarchichal/ClassHierarchy.java
@@ -1,14 +1,95 @@
 package net.alexweinert.coolc.representations.cool.program.hierarchichal;
 
+import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
+import net.alexweinert.coolc.representations.cool.program.parsed.ClassNode;
+import net.alexweinert.coolc.representations.cool.program.parsed.ParsedProgramVisitor;
 import net.alexweinert.coolc.representations.cool.program.parsed.Program;
 import net.alexweinert.coolc.representations.cool.symboltables.IdSymbol;
+import net.alexweinert.coolc.representations.cool.symboltables.IdTable;
 
 public class ClassHierarchy {
+
+    private static class Builder {
+        private class Pair<T, U> {
+            final private T x;
+            final private U y;
+
+            public Pair(T x, U y) {
+                this.x = x;
+                this.y = y;
+            }
+        }
+
+        final private Collection<Pair<IdSymbol, IdSymbol>> inheritanceInfo = new HashSet<>();
+
+        void addInheritance(IdSymbol childClass, IdSymbol parentClass) {
+            this.inheritanceInfo.add(new Pair<>(childClass, parentClass));
+        }
+
+        ClassHierarchy buildHierarchy() {
+            final Map<IdSymbol, List<IdSymbol>> hierarchy = new HashMap<>();
+            hierarchy.put(IdTable.getInstance().getObjectSymbol(),
+                    Collections.singletonList(IdTable.getInstance().getObjectSymbol()));
+            for (Pair<IdSymbol, IdSymbol> singleInheritance : this.inheritanceInfo) {
+                final List<IdSymbol> ancestors = this.computeAncestors(singleInheritance.x);
+                hierarchy.put(singleInheritance.x, ancestors);
+            }
+            return new ClassHierarchy(hierarchy);
+        }
+
+        private List<IdSymbol> computeAncestors(IdSymbol root) {
+            final List<IdSymbol> ancestors = new LinkedList<>();
+            ancestors.add(root);
+            Pair<IdSymbol, IdSymbol> singleInheritance = this.findSingleInheritance(root);
+            while (singleInheritance != null) {
+                ancestors.add(singleInheritance.y);
+                singleInheritance = this.findSingleInheritance(singleInheritance.y);
+            }
+            return ancestors;
+        }
+
+        private Pair<IdSymbol, IdSymbol> findSingleInheritance(IdSymbol lhs) {
+            for (Pair<IdSymbol, IdSymbol> singleInheritance : this.inheritanceInfo) {
+                if (singleInheritance.x.equals(lhs)) {
+                    return singleInheritance;
+                }
+            }
+            return null;
+        }
+
+    }
+
+    private static class Factory extends ParsedProgramVisitor {
+        final private Builder builder = new Builder();
+
+        public static ClassHierarchy buildHierarchy(Program ast) {
+            final Factory visitor = new Factory();
+            ast.acceptVisitor(visitor);
+            return visitor.builder.buildHierarchy();
+        }
+
+        @Override
+        public void visitProgramPreorder(Program program) {
+            // Install predefined classes
+            this.builder.addInheritance(IdTable.getInstance().getIOSymbol(), IdTable.getInstance().getObjectSymbol());
+            this.builder.addInheritance(IdTable.getInstance().getIntSymbol(), IdTable.getInstance().getObjectSymbol());
+            this.builder.addInheritance(IdTable.getInstance().getStringSymbol(), IdTable.getInstance()
+                    .getObjectSymbol());
+            this.builder.addInheritance(IdTable.getInstance().getBoolSymbol(), IdTable.getInstance().getObjectSymbol());
+        }
+
+        public void visitClassPreorder(ClassNode classNode) {
+            this.builder.addInheritance(classNode.getIdentifier(), classNode.getParent());
+        }
+
+    }
 
     final private Map<IdSymbol, List<IdSymbol>> parentClasses;
 
@@ -17,7 +98,7 @@ public class ClassHierarchy {
      * from base classes
      */
     public static ClassHierarchy create(Program program) {
-        return ClassHierarchyFactory.buildHierarchy(program);
+        return Factory.buildHierarchy(program);
     }
 
     /**
