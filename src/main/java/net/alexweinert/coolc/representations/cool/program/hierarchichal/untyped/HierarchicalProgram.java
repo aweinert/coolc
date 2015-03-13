7c98823efb878ad072c64ec33a614b4aa2125431
Alexander Weinert <alexander.weinert@rwth-aachen.de>
2015-03-13 18:16:09 +0100

    Rename Program -> HierarchicalProgram

--- src/main/java/net/alexweinert/coolc/representations/cool/program/hierarchichal/untyped/HierarchicalProgram.java
+++ src/main/java/net/alexweinert/coolc/representations/cool/program/hierarchichal/untyped/HierarchicalProgram.java
@@ -7,10 +7,10 @@ import net.alexweinert.coolc.representations.cool.symboltables.SymbolTables;
 /**
  * The complete program. Root node of a well-formed AST
  */
-public class Program extends net.alexweinert.coolc.representations.cool.program.parsed.Program {
+public class HierarchicalProgram extends net.alexweinert.coolc.representations.cool.program.parsed.Program {
     private final ClassHierarchy hierarchy;
 
-    public Program(String filename, int lineNumber, Classes classes, SymbolTables symbolTables, ClassHierarchy hierarchy) {
+    public HierarchicalProgram(String filename, int lineNumber, Classes classes, SymbolTables symbolTables, ClassHierarchy hierarchy) {
         super(filename, lineNumber, classes, symbolTables);
         this.hierarchy = hierarchy;
     }
