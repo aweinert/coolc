24036640bc71e019198e96b25478ff02b8137297
Alexander Weinert <alexander.weinert@rwth-aachen.de>
2015-03-20 10:34:16 +0100

    Rethrow exception in parser if necessary

--- src/main/java/net/alexweinert/coolc/processors/cool/frontend/CoolParser.java
+++ src/main/java/net/alexweinert/coolc/processors/cool/frontend/CoolParser.java
@@ -3,6 +3,7 @@ package net.alexweinert.coolc.processors.cool.frontend;
 import java.io.Reader;
 
 import net.alexweinert.coolc.infrastructure.Processor;
+import net.alexweinert.coolc.infrastructure.ProcessorException;
 import net.alexweinert.coolc.processors.cool.frontend.parser.Parser;
 import net.alexweinert.coolc.processors.cool.frontend.parser.ParserFactory;
 import net.alexweinert.coolc.representations.cool.ast.Program;
@@ -10,13 +11,13 @@ import net.alexweinert.coolc.representations.cool.ast.Program;
 public class CoolParser extends Processor<Reader, Program> {
 
     @Override
-    public Program process(final Reader reader) {
+    public Program process(final Reader reader) throws ProcessorException {
         final Program program;
         try {
             final Parser parser = ParserFactory.createParserFromReader(reader);
             program = (Program) parser.parse().value;
-        } catch (Throwable e) {
-            return null;
+        } catch (Exception e) {
+            throw new ProcessorException(e);
         }
 
         return program;
