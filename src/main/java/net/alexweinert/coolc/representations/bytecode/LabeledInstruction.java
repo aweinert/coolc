a91226e10743c7d98ed01e652999566e5bc68260
Alexander Weinert <alexander.weinert@rwth-aachen.de>
2015-03-27 18:13:34 +0100

    Implement Instruction.Factory

--- src/main/java/net/alexweinert/coolc/representations/bytecode/LabeledInstruction.java
+++ src/main/java/net/alexweinert/coolc/representations/bytecode/LabeledInstruction.java
@@ -4,121 +4,147 @@ import java.util.List;
 
 public abstract class LabeledInstruction {
     public static class Factory {
+        private String label = null;
 
         public void setLabel(String label) {
-            // TODO Auto-generated method stub
+            this.label = label;
         }
 
         public LabeledInstruction buildLoadVoid(String target) {
-            // TODO Auto-generated method stub
-            return null;
+            final LabeledInstruction returnValue = new LoadVoidInstruction(this.label, target);
+            this.label = null;
+            return returnValue;
         }
 
         public LabeledInstruction buildLoadBool(String target, boolean value) {
-            // TODO Auto-generated method stub
-            return null;
+            final LabeledInstruction returnValue = new LoadBoolInstruction(this.label, target, value);
+            this.label = null;
+            return returnValue;
         }
 
         public LabeledInstruction buildLoadInt(String target, int value) {
-            // TODO Auto-generated method stub
-            return null;
+            final LabeledInstruction returnValue = new LoadIntInstruction(this.label, target, value);
+            this.label = null;
+            return returnValue;
         }
 
         public LabeledInstruction buildLoadString(String target, String value) {
-            // TODO Auto-generated method stub
-            return null;
+            final LabeledInstruction returnValue = new LoadStringInstruction(this.label, target, value);
+            this.label = null;
+            return returnValue;
         }
 
         public LabeledInstruction buildAssign(String target, String source) {
-            // TODO Auto-generated method stub
-            return null;
+            final LabeledInstruction returnValue = new AssignInstruction(this.label, target, source);
+            this.label = null;
+            return returnValue;
         }
 
         public LabeledInstruction buildAdd(String result, String lhs, String rhs) {
-            // TODO Auto-generated method stub
-            return null;
+            final LabeledInstruction returnValue = new AddInstruction(this.label, result, lhs, rhs);
+            this.label = null;
+            return returnValue;
         }
 
         public LabeledInstruction buildSub(String result, String lhs, String rhs) {
-            // TODO Auto-generated method stub
-            return null;
+            final LabeledInstruction returnValue = new SubInstruction(this.label, result, lhs, rhs);
+            this.label = null;
+            return returnValue;
         }
 
         public LabeledInstruction buildMul(String result, String lhs, String rhs) {
-            // TODO Auto-generated method stub
-            return null;
+            final LabeledInstruction returnValue = new MulInstruction(this.label, result, lhs, rhs);
+            this.label = null;
+            return returnValue;
         }
 
         public LabeledInstruction buildDiv(String result, String lhs, String rhs) {
-            // TODO Auto-generated method stub
-            return null;
+            final LabeledInstruction returnValue = new DivInstruction(this.label, result, lhs, rhs);
+            this.label = null;
+            return returnValue;
         }
 
         public LabeledInstruction buildArithNeg(String result, String arg) {
-            // TODO Auto-generated method stub
-            return null;
+            final LabeledInstruction returnValue = new ArithNegInstruction(this.label, result, arg);
+            this.label = null;
+            return returnValue;
         }
 
         public LabeledInstruction buildIsVoid(String result, String arg) {
-            // TODO Auto-generated method stub
-            return null;
+            final LabeledInstruction returnValue = new IsVoidInstruction(this.label, result, arg);
+            this.label = null;
+            return returnValue;
         }
 
         public LabeledInstruction buildBoolNeg(String result, String arg) {
-            // TODO Auto-generated method stub
-            return null;
+            final LabeledInstruction returnValue = new BoolNegInstruction(this.label, result, arg);
+            this.label = null;
+            return returnValue;
         }
 
         public LabeledInstruction buildLt(String result, String lhs, String rhs) {
-            // TODO Auto-generated method stub
-            return null;
+            final LabeledInstruction returnValue = new LtInstruction(this.label, result, lhs, rhs);
+            this.label = null;
+            return returnValue;
         }
 
         public LabeledInstruction buildLte(String result, String lhs, String rhs) {
-            // TODO Auto-generated method stub
-            return null;
+            final LabeledInstruction returnValue = new LteInstruction(this.label, result, lhs, rhs);
+            this.label = null;
+            return returnValue;
         }
 
         public LabeledInstruction buildEq(String result, String lhs, String rhs) {
-            // TODO Auto-generated method stub
-            return null;
+            final LabeledInstruction returnValue = new EqInstruction(this.label, result, lhs, rhs);
+            this.label = null;
+            return returnValue;
         }
 
-        public LabeledInstruction buildNew(String returnVariable, String string) {
-            // TODO Auto-generated method stub
-            return null;
+        public LabeledInstruction buildNew(String returnVariable, String type) {
+            final LabeledInstruction returnValue = new NewInstruction(this.label, result, type);
+            this.label = null;
+            return returnValue;
         }
 
-        public LabeledInstruction buildFunctionCall(String resultVariable, String dispatchVariable, String string,
+        public LabeledInstruction buildFunctionCall(String resultVariable, String dispatchVariable, String functionId,
                 List<String> arguments) {
-            // TODO Auto-generated method stub
-            return null;
+            final LabeledInstruction returnValue = new FunctionCallInstruction(this.label, resultVariable,
+                    dispatchVariable, functionId, arguments);
+            this.label = null;
+            return returnValue;
         }
 
         public LabeledInstruction buildStaticFunctionCall(String resultVariable, String dispatchVariable,
-                String string, String string2, List<String> arguments) {
-            // TODO Auto-generated method stub
-            return null;
+                String staticType, String functionId, List<String> arguments) {
+            final LabeledInstruction returnValue = new FunctionCallInstruction(this.label, resultVariable,
+                    dispatchVariable, staticType, functionId, arguments);
+            this.label = null;
+            return returnValue;
         }
 
-        public LabeledInstruction buildBranch(String afterLabel) {
-            // TODO Auto-generated method stub
-            return null;
+        public LabeledInstruction buildBranch(String target) {
+            final LabeledInstruction returnValue = new BranchInstruction(this.label, target);
+            this.label = null;
+            return returnValue;
         }
 
-        public LabeledInstruction buildBranchIfFalse(String conditionVariable, String afterLabel) {
-            // TODO Auto-generated method stub
-            return null;
+        public LabeledInstruction buildBranchIfFalse(String conditionVariable, String target) {
+            final LabeledInstruction returnValue = new BranchIfFalseInstruction(this.label, conditionVariable, target);
+            this.label = null;
+            return returnValue;
         }
 
-        public LabeledInstruction buildBranchIfNotInstanceOf(String expressionVariable, String nextLabel) {
-            // TODO Auto-generated method stub
-            return null;
+        public LabeledInstruction buildBranchIfNotInstanceOf(String expressionVariable, String target) {
+            final LabeledInstruction returnValue = new BranchIfNotInstanceOfInstruction(this.label, expressionVariable,
+                    target);
+            this.label = null;
+            return returnValue;
         }
 
         public LabeledInstruction buildReturn(String returnVariable) {
-            // TODO Auto-generated method stub
-            return null;
+            final LabeledInstruction returnValue = new ReturnInstruction(this.label, returnVariable);
+            this.label = null;
+            return returnValue;
         }
 
     }
