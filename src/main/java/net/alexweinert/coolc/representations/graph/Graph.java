04e977bcea262e28f955bc91c44e99129fb08277
Alexander Weinert <alexander.weinert@rwth-aachen.de>
2015-03-29 21:02:50 +0200

    Add identifier to Graph

--- src/main/java/net/alexweinert/coolc/representations/graph/Graph.java
+++ src/main/java/net/alexweinert/coolc/representations/graph/Graph.java
@@ -5,8 +5,14 @@ import java.util.Set;
 
 public class Graph<T> {
     public static class Builder<T> {
+
+        private final String identifier;
         private Set<Node<T>> nodes = new HashSet<>();
         private Set<Edge<T>> edges = new HashSet<>();
+
+        public Builder(String identifier) {
+            this.identifier = identifier;
+        }
 
         public void addNode(T value) {
             this.nodes.add(new Node<>(value));
@@ -21,16 +27,22 @@ public class Graph<T> {
         }
 
         public Graph<T> build() {
-            return new Graph<>(this.nodes, this.edges);
+            return new Graph<>(this.identifier, this.nodes, this.edges);
         }
     }
 
+    private final String identifier;
     private final Set<Node<T>> nodes;
     private final Set<Edge<T>> edges;
 
-    public Graph(Set<Node<T>> nodes, Set<Edge<T>> edges) {
+    public Graph(String identifier, Set<Node<T>> nodes, Set<Edge<T>> edges) {
+        this.identifier = identifier;
         this.nodes = nodes;
         this.edges = edges;
+    }
+
+    public String getIdentifier() {
+        return this.identifier;
     }
 
     public Set<Node<T>> getNodes() {
