188878826d147eac155bd1f403460f5afa85dd2d
Alexander Weinert <alexander.weinert@rwth-aachen.de>
2015-03-30 00:47:23 +0200

    Implement very simple unparsing of class files

--- src/main/java/net/alexweinert/coolc/processors/jbc/JbcToFileProcessor.java
+++ src/main/java/net/alexweinert/coolc/processors/jbc/JbcToFileProcessor.java
@@ -1,5 +1,9 @@
 package net.alexweinert.coolc.processors.jbc;
 
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
 import java.nio.file.Path;
 import java.util.Collection;
 
@@ -17,8 +21,42 @@ public class JbcToFileProcessor implements Backend<Collection<ClassFile>> {
 
     @Override
     public void process(Collection<ClassFile> input) throws ProcessorException {
-        // TODO Auto-generated method stub
+        try {
+            for (ClassFile file : input) {
+                final FileOutputStream writer = new FileOutputStream(this.folderPath.resolve(
+                        file.getClassId() + ".class").toString());
 
+                writer.write(new byte[] { (byte) 0xca, (byte) 0xfe, (byte) 0xba, (byte) 0xbe });
+                writer.write(file.getMinorVersion());
+                writer.write(file.getMajorVersion());
+
+                writer.write(file.getConstantsCount());
+                writer.write(file.getConstants());
+
+                writer.write(file.getAccessFlags());
+                writer.write(this.splitChar(file.getThisClassIndex()));
+                writer.write(this.splitChar(file.getSuperClassIndex()));
+
+                writer.write(new byte[] { 0x00, 0x00 });
+                writer.write(file.getInterfaces());
+
+                writer.write(new byte[] { 0x00, 0x00 });
+                writer.write(file.getFields());
+
+                writer.write(new byte[] { 0x00, 0x00 });
+                writer.write(file.getMethods());
+
+                writer.write(new byte[] { 0x00, 0x00 });
+                writer.write(file.getAttributes());
+
+                writer.close();
+            }
+        } catch (IOException e) {
+            throw new ProcessorException(e);
+        }
     }
 
+    private byte[] splitChar(final char value) {
+        return new byte[] { (byte) ((value >>> 8) & 0xFF), (byte) (value & 0xFF) };
+    }
 }
--- src/main/java/net/alexweinert/coolc/representations/jbc/ClassFile.java
+++ src/main/java/net/alexweinert/coolc/representations/jbc/ClassFile.java
@@ -1,5 +1,8 @@
 package net.alexweinert.coolc.representations.jbc;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class ClassFile {
     public static class Builder {
         private final String id;
@@ -15,7 +18,110 @@ public class ClassFile {
         }
 
         public ClassFile build() {
-            return new ClassFile();
+            final List<Byte> constants = new LinkedList<>();
+            constants.add(Byte.valueOf((byte) 0x07)); // Class-tag
+            constants.add(Byte.valueOf((byte) 0x00));
+            constants.add(Byte.valueOf((byte) 0x02));
+
+            constants.add(Byte.valueOf((byte) 0x01)); // UTF8-tag
+            constants.add(Byte.valueOf((byte) 0x00)); // String length (high)
+            constants.add(Byte.valueOf((byte) 0x03)); // String length (low)
+            for (byte character : this.id.getBytes()) {
+                constants.add(character);
+            }
+
+            constants.add(Byte.valueOf((byte) 0x07)); // Class-tag
+            constants.add(Byte.valueOf((byte) 0x00));
+            constants.add(Byte.valueOf((byte) 0x04));
+
+            constants.add(Byte.valueOf((byte) 0x01)); // UTF8-tag
+            constants.add(Byte.valueOf((byte) 0x00)); // String length (high)
+            constants.add(Byte.valueOf((byte) 0x06)); // String length (low)
+            for (byte character : this.parent.getBytes()) {
+                constants.add(character);
+            }
+
+            final byte[] constantsArray = new byte[constants.size()];
+            for (int i = 0; i < constants.size(); ++i) {
+                constantsArray[i] = constants.get(i);
+            }
+            return new ClassFile(constantsArray, (char) 0x01, (char) 0x03);
         }
     }
+
+    private final byte[] constants;
+
+    private final char thisClassIndex;
+    private final char superClassIndex;
+
+    private ClassFile(byte[] constants, char thisClassIndex, char superClassIndex) {
+        this.constants = constants;
+        this.thisClassIndex = thisClassIndex;
+        this.superClassIndex = superClassIndex;
+    }
+
+    public byte[] getMinorVersion() {
+        return new byte[] { 0x00, 0x00 };
+    }
+
+    public byte[] getMajorVersion() {
+        return new byte[] { 0x00, 0x33 };
+    }
+
+    public String getClassId() {
+        return "blub";
+    }
+
+    public byte[] getConstantsCount() {
+        return new byte[] { 0x00, 0x05 };
+    }
+
+    public byte[] getConstants() {
+        return this.constants;
+    }
+
+    public byte[] getAccessFlags() {
+        return new byte[] { 0x00, 0x00 };
+    }
+
+    public char getThisClassIndex() {
+        return thisClassIndex;
+    }
+
+    public char getSuperClassIndex() {
+        return superClassIndex;
+    }
+
+    public byte[] getInterfacesCount() {
+        return new byte[] { 0x00, 0x00 };
+    }
+
+    public byte[] getInterfaces() {
+        return new byte[0];
+    }
+
+    public byte[] getFieldsCount() {
+        return new byte[] { 0x00 };
+    }
+
+    public byte[] getFields() {
+        return new byte[0];
+    }
+
+    public byte[] getMethodsCount() {
+        return new byte[] { 0x00 };
+    }
+
+    public byte[] getMethods() {
+        return new byte[0];
+    }
+
+    public byte[] getAttributesCount() {
+        return new byte[] { 0x00 };
+    }
+
+    public byte[] getAttributes() {
+        return new byte[0];
+    }
+
 }
