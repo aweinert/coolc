361a39cf0e401c93fc136648c55c54e15b8bdd19
Alexander Weinert <alexander.weinert@rwth-aachen.de>
2015-04-04 18:08:25 +0200

    Implement JavaToFilesProcessor

--- src/main/java/net/alexweinert/coolc/processors/java/tofiles/JavaToFilesProcessor.java
+++ src/main/java/net/alexweinert/coolc/processors/java/tofiles/JavaToFilesProcessor.java
@@ -1,18 +1,28 @@
 package net.alexweinert.coolc.processors.java.tofiles;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Collection;
+import java.util.HashSet;
 
 import net.alexweinert.coolc.infrastructure.Processor;
 import net.alexweinert.coolc.infrastructure.ProcessorException;
 import net.alexweinert.coolc.representations.io.File;
+import net.alexweinert.coolc.representations.java.JavaClass;
 import net.alexweinert.coolc.representations.java.JavaProgram;
 
 public class JavaToFilesProcessor extends Processor<JavaProgram, Collection<File>> {
 
     @Override
     public Collection<File> process(JavaProgram input) throws ProcessorException {
-        // TODO Auto-generated method stub
-        return null;
+        final Collection<File> returnValue = new HashSet<>();
+        for (JavaClass javaClass : input.getClasses()) {
+            final Path path = Paths.get(javaClass.getIdentifier().toString() + ".java");
+            final File.Builder builder = new File.Builder(path);
+            builder.appendContent(javaClass.getDefinition().getBytes());
+            returnValue.add(builder.build());
+        }
+        return returnValue;
     }
 
 }
