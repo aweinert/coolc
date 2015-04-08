4b342599ec8278726776bb69fce6b9237a8771dd
Alexander Weinert <alexander.weinert@rwth-aachen.de>
2015-04-08 23:28:09 +0200

    Build fields in BytecodeToJbcProcessor

--- src/main/java/net/alexweinert/coolc/processors/bytecode/tojbc/BytecodeToJbcProcessor.java
+++ src/main/java/net/alexweinert/coolc/processors/bytecode/tojbc/BytecodeToJbcProcessor.java
@@ -6,7 +6,9 @@ import java.util.List;
 
 import net.alexweinert.coolc.infrastructure.Processor;
 import net.alexweinert.coolc.infrastructure.ProcessorException;
+import net.alexweinert.coolc.representations.bytecode.Attribute;
 import net.alexweinert.coolc.representations.bytecode.ByteClass;
+import net.alexweinert.coolc.representations.jbc.FieldEntry;
 import net.alexweinert.coolc.representations.jbc.JbcClass;
 
 public class BytecodeToJbcProcessor extends Processor<List<ByteClass>, Collection<JbcClass>> {
@@ -16,6 +18,14 @@ public class BytecodeToJbcProcessor extends Processor<List<ByteClass>, Collectio
         final Collection<JbcClass> returnValue = new HashSet<>();
         for (ByteClass byteClass : input) {
             final JbcClass.Builder builder = new JbcClass.Builder(byteClass.getId(), byteClass.getParent());
+            for (Attribute attr : byteClass.getAttributes()) {
+                final char nameIndex = builder
+                        .addConstant(builder.getConstantBuilder().buildUtf8Constant(attr.getId()));
+                final char descriptorIndex = builder.addConstant(builder.getConstantBuilder().buildUtf8Constant(
+                        "L" + attr.getType() + ";"));
+                builder.addField(builder.getFieldBuilder(nameIndex, descriptorIndex).build());
+            }
+
             returnValue.add(builder.build());
         }
 
