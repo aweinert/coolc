5b8e1d943efbcc859edbd60d7792e5af1a48d10c
Alexander Weinert <alexander.weinert@rwth-aachen.de>
2015-04-09 21:51:51 +0200

    Build methods in BytecodeToJbcProcessor

--- src/main/java/net/alexweinert/coolc/processors/bytecode/tojbc/BytecodeToJbcProcessor.java
+++ src/main/java/net/alexweinert/coolc/processors/bytecode/tojbc/BytecodeToJbcProcessor.java
@@ -8,7 +8,8 @@ import net.alexweinert.coolc.infrastructure.Processor;
 import net.alexweinert.coolc.infrastructure.ProcessorException;
 import net.alexweinert.coolc.representations.bytecode.Attribute;
 import net.alexweinert.coolc.representations.bytecode.ByteClass;
-import net.alexweinert.coolc.representations.jbc.FieldEntry;
+import net.alexweinert.coolc.representations.bytecode.Method;
+import net.alexweinert.coolc.representations.bytecode.TypedId;
 import net.alexweinert.coolc.representations.jbc.JbcClass;
 
 public class BytecodeToJbcProcessor extends Processor<List<ByteClass>, Collection<JbcClass>> {
@@ -24,6 +25,25 @@ public class BytecodeToJbcProcessor extends Processor<List<ByteClass>, Collectio
                 final char descriptorIndex = builder.addConstant(builder.getConstantBuilder().buildUtf8Constant(
                         "L" + attr.getType() + ";"));
                 builder.addField(builder.getFieldBuilder(nameIndex, descriptorIndex).build());
+            }
+
+            for (Method method : byteClass.getMethods()) {
+                final char nameIndex = builder.addConstant(builder.getConstantBuilder().buildUtf8Constant(
+                        method.getId()));
+
+                final StringBuilder descriptorBuilder = new StringBuilder("(");
+                for (TypedId id : method.getParameters()) {
+                    descriptorBuilder.append("L");
+                    descriptorBuilder.append(id.getType());
+                    descriptorBuilder.append(";");
+                }
+                descriptorBuilder.append(")");
+                descriptorBuilder.append("L");
+                descriptorBuilder.append(method.getReturnType());
+                descriptorBuilder.append(";");
+                final char descriptorIndex = builder.addConstant(builder.getConstantBuilder().buildUtf8Constant(
+                        descriptorBuilder.toString()));
+                builder.addMethod(builder.getMethodBuilder(nameIndex, descriptorIndex).build());
             }
 
             returnValue.add(builder.build());
