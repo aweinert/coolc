b8284bb0bda7a0856ccbd5269738963e3dc3326f
Alexander Weinert <alexander.weinert@rwth-aachen.de>
2015-04-12 21:23:01 +0200

    Refactor BytecodeToJbcProcessor

--- src/main/java/net/alexweinert/coolc/processors/bytecode/tojbc/BytecodeToJbcProcessor.java
+++ src/main/java/net/alexweinert/coolc/processors/bytecode/tojbc/BytecodeToJbcProcessor.java
@@ -10,7 +10,9 @@ import net.alexweinert.coolc.representations.bytecode.Attribute;
 import net.alexweinert.coolc.representations.bytecode.ByteClass;
 import net.alexweinert.coolc.representations.bytecode.Method;
 import net.alexweinert.coolc.representations.bytecode.TypedId;
+import net.alexweinert.coolc.representations.jbc.FieldEntry;
 import net.alexweinert.coolc.representations.jbc.JbcClass;
+import net.alexweinert.coolc.representations.jbc.MethodEntry;
 
 public class BytecodeToJbcProcessor extends Processor<List<ByteClass>, Collection<JbcClass>> {
 
@@ -20,35 +22,49 @@ public class BytecodeToJbcProcessor extends Processor<List<ByteClass>, Collectio
         for (ByteClass byteClass : input) {
             final JbcClass.Builder builder = new JbcClass.Builder(byteClass.getId(), byteClass.getParent());
             for (Attribute attr : byteClass.getAttributes()) {
-                final char nameIndex = builder
-                        .addConstant(builder.getConstantBuilder().buildUtf8Constant(attr.getId()));
-                final char descriptorIndex = builder.addConstant(builder.getConstantBuilder().buildUtf8Constant(
-                        "L" + attr.getType() + ";"));
-                builder.addField(builder.getFieldBuilder(nameIndex, descriptorIndex).build());
+                builder.addField(this.buildField(builder, attr));
             }
 
             for (Method method : byteClass.getMethods()) {
-                final char nameIndex = builder.addConstant(builder.getConstantBuilder().buildUtf8Constant(
-                        method.getId()));
-
-                final StringBuilder descriptorBuilder = new StringBuilder("(");
-                for (TypedId id : method.getParameters()) {
-                    descriptorBuilder.append("L");
-                    descriptorBuilder.append(id.getType());
-                    descriptorBuilder.append(";");
-                }
-                descriptorBuilder.append(")");
-                descriptorBuilder.append("L");
-                descriptorBuilder.append(method.getReturnType());
-                descriptorBuilder.append(";");
-                final char descriptorIndex = builder.addConstant(builder.getConstantBuilder().buildUtf8Constant(
-                        descriptorBuilder.toString()));
-                builder.addMethod(builder.getMethodBuilder(nameIndex, descriptorIndex).build());
+                final MethodEntry jbcMethod = buildMethod(builder, method);
+                builder.addMethod(jbcMethod);
             }
 
             returnValue.add(builder.build());
         }
 
         return returnValue;
+    }
+
+    private FieldEntry buildField(final JbcClass.Builder builder, Attribute attr) {
+        final char nameIndex = builder.addConstant(builder.getConstantBuilder().buildUtf8Constant(attr.getId()));
+        final char descriptorIndex = builder.addConstant(builder.getConstantBuilder().buildUtf8Constant(
+                "L" + attr.getType() + ";"));
+        final FieldEntry field = builder.getFieldBuilder(nameIndex, descriptorIndex).build();
+        return field;
+    }
+
+    private MethodEntry buildMethod(final JbcClass.Builder builder, Method method) {
+        final char nameIndex = builder.addConstant(builder.getConstantBuilder().buildUtf8Constant(method.getId()));
+
+        final String descriptor = buildMethodDescriptor(method);
+        final char descriptorIndex = builder.addConstant(builder.getConstantBuilder().buildUtf8Constant(descriptor));
+
+        final MethodEntry jbcMethod = builder.getMethodBuilder(nameIndex, descriptorIndex).build();
+        return jbcMethod;
+    }
+
+    private String buildMethodDescriptor(Method method) {
+        final StringBuilder descriptorBuilder = new StringBuilder("(");
+        for (TypedId id : method.getParameters()) {
+            descriptorBuilder.append("L");
+            descriptorBuilder.append(id.getType());
+            descriptorBuilder.append(";");
+        }
+        descriptorBuilder.append(")");
+        descriptorBuilder.append("L");
+        descriptorBuilder.append(method.getReturnType());
+        descriptorBuilder.append(";");
+        return descriptorBuilder.toString();
     }
 }
