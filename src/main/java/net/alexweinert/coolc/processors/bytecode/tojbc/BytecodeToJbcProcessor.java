4a7f7b584340abf0249258bb973bbf0102aae6e7
Alexander Weinert <alexander.weinert@rwth-aachen.de>
2015-04-29 17:44:17 +0200

    Account for this-variable in Java

--- src/main/java/net/alexweinert/coolc/processors/bytecode/tojbc/BytecodeToJbcProcessor.java
+++ src/main/java/net/alexweinert/coolc/processors/bytecode/tojbc/BytecodeToJbcProcessor.java
@@ -55,8 +55,8 @@ public class BytecodeToJbcProcessor extends Processor<List<ByteClass>, Collectio
 
         final MethodEntry.Builder methodBuilder = builder.getMethodBuilder(nameIndex, descriptorIndex);
         final CodeAttribute.Builder codeBuilder = new CodeAttribute.Builder(builder.addConstant(builder
-                .getConstantBuilder().buildUtf8Constant("Code")), (char) 255,
-                (char) (method.getParameters().size() + method.getLocalVars().size()));
+                .getConstantBuilder().buildUtf8Constant("Code")), (char) 255, (char) (method.getParameters().size()
+                + method.getLocalVars().size() + 1));
 
         final BytecodeOpToJbcOpConverter converter = BytecodeOpToJbcOpConverter.create(method.getParameters(),
                 method.getLocalVars(), builder, JbcEncoding.createStandardEncoding());
