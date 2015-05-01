dbb7e91e1faf0dff8922696883f3553e316f482c
Alexander Weinert <alexander.weinert@rwth-aachen.de>
2015-05-01 12:29:12 +0200

    Implement main-method in Main-class

--- src/main/java/net/alexweinert/coolc/processors/bytecode/tojbc/BytecodeToJbcProcessor.java
+++ src/main/java/net/alexweinert/coolc/processors/bytecode/tojbc/BytecodeToJbcProcessor.java
@@ -23,7 +23,8 @@ public class BytecodeToJbcProcessor extends Processor<List<ByteClass>, Collectio
     public Collection<JbcClass> process(List<ByteClass> input) throws ProcessorException {
         final Collection<JbcClass> returnValue = new HashSet<>();
         for (ByteClass byteClass : input) {
-            final JbcClass.Builder builder = new JbcClass.Builder(byteClass.getId(), byteClass.getParent());
+            final JbcClass.Builder builder = new JbcClass.Builder("Cool" + byteClass.getId(), "Cool"
+                    + byteClass.getParent());
             for (Attribute attr : byteClass.getAttributes()) {
                 builder.addField(this.buildField(builder, attr));
             }
@@ -33,10 +34,52 @@ public class BytecodeToJbcProcessor extends Processor<List<ByteClass>, Collectio
                 builder.addMethod(jbcMethod);
             }
 
+            if (byteClass.getId().equals("Main")) {
+                builder.addMethod(buildMainMethod(builder));
+            }
+
             returnValue.add(builder.build());
         }
 
         return returnValue;
+    }
+
+    private MethodEntry buildMainMethod(JbcClass.Builder builder) {
+        final char nameIndex = builder.addConstant(builder.getConstantBuilder().buildUtf8Constant("main"));
+        final char descriptorIndex = builder.addConstant(builder.getConstantBuilder().buildUtf8Constant(
+                "([Ljava/lang/String;)V"));
+        final MethodEntry.Builder methodBuilder = builder.getMethodBuilder(nameIndex, descriptorIndex);
+        methodBuilder.setStatic(true);
+        final OpCodeAssembler assembler = new OpCodeAssembler(new JbcEncoding());
+        final char classRefNameIndex = builder.addConstant(builder.getConstantBuilder().buildUtf8Constant("CoolMain"));
+        final char classRefIndex = builder.addConstant(builder.getConstantBuilder().buildClassConstant(
+                classRefNameIndex));
+        assembler.addNew(classRefIndex);
+        assembler.addDup();
+        final char initMethodNameId = builder.addConstant(builder.getConstantBuilder().buildUtf8Constant("<init>"));
+        final char initMethodDescId = builder.addConstant(builder.getConstantBuilder().buildUtf8Constant("()V"));
+        final char initMethodNameAndDescId = builder.addConstant(builder.getConstantBuilder().buildNameAndType(
+                initMethodNameId, initMethodDescId));
+        final char initMethodRefId = builder.addConstant(builder.getConstantBuilder().buildMethodRef(classRefIndex,
+                initMethodNameAndDescId));
+        assembler.addInvokeSpecial(initMethodRefId);
+
+        final char mainMethodNameId = builder.addConstant(builder.getConstantBuilder().buildUtf8Constant("main"));
+        final char mainMethodDescId = builder.addConstant(builder.getConstantBuilder().buildUtf8Constant(
+                "()LCoolObject;"));
+        final char mainMethodTypeId = builder.addConstant(builder.getConstantBuilder().buildNameAndType(
+                mainMethodNameId, mainMethodDescId));
+        final char mainMethodRefId = builder.addConstant(builder.getConstantBuilder().buildMethodRef(classRefIndex,
+                mainMethodTypeId));
+        assembler.addInvokeVirtual(mainMethodRefId);
+        assembler.addPop();
+        assembler.addReturn();
+
+        final char codeStringId = builder.addConstant(builder.getConstantBuilder().buildUtf8Constant("Code"));
+        final CodeAttribute.Builder codeBuilder = new CodeAttribute.Builder(codeStringId, (char) 2, (char) 2);
+        codeBuilder.setCode(assembler.assemble());
+        methodBuilder.addAttribute(codeBuilder.build(new JbcEncoding()));
+        return methodBuilder.build();
     }
 
     private FieldEntry buildField(final JbcClass.Builder builder, Attribute attr) {
