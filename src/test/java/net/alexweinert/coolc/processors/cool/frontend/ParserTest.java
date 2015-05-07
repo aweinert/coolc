09012e7a76381410ffb8efcc008696fcb26120e9
Alexander Weinert <alexander.weinert@rwth-aachen.de>
2015-05-07 19:44:11 +0200

    Create Parser correctly when testing

--- src/test/java/net/alexweinert/coolc/processors/cool/frontend/ParserTest.java
+++ src/test/java/net/alexweinert/coolc/processors/cool/frontend/ParserTest.java
@@ -1,13 +1,10 @@
 package net.alexweinert.coolc.processors.cool.frontend;
 
-import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.net.URI;
-import java.net.URISyntaxException;
 import java.nio.file.Paths;
 import java.util.Collections;
 
-import net.alexweinert.coolc.processors.cool.frontend.parser.Parser;
-import net.alexweinert.coolc.processors.cool.frontend.parser.ParserFactory;
 import net.alexweinert.coolc.representations.cool.ast.ClassNode;
 import net.alexweinert.coolc.representations.cool.ast.Classes;
 import net.alexweinert.coolc.representations.cool.ast.Features;
@@ -23,8 +20,9 @@ public class ParserTest {
     public void testClassParse() throws Exception {
         final URI uri = this.getClass().getResource("/parser/positive/class-a.cl").toURI();
         final String path = Paths.get(uri).toString();
-        final Parser parser = ParserFactory.createParserForFile(path);
-        final Program actualProgram = (Program) parser.parse().value;
+
+        final CoolParser parser = new CoolParser();
+        final Program actualProgram = parser.process(new FileReader(path));
         final Program expectedProgram = new Program(path, 1, new Classes(path, 1,
                 Collections.singletonList(new ClassNode(path, 1, IdTable.getInstance().addString("A"), IdTable
                         .getInstance().addString("Object"), new Features(path, 1, Collections.EMPTY_LIST)))));
