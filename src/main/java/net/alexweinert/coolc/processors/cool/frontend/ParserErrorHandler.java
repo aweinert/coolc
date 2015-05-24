22c1f918ee20a5dcf34d9bc534739b8242e517ae
Alexander Weinert <alexander.weinert@rwth-aachen.de>
2015-05-24 13:29:11 +0200

    Add skeleton of ParserErrorHandler

---
+++ src/main/java/net/alexweinert/coolc/processors/cool/frontend/ParserErrorHandler.java
@@ -0,0 +1,9 @@
+package net.alexweinert.coolc.processors.cool.frontend;
+
+import java.util.List;
+
+public class ParserErrorHandler {
+    public void parseError(String filename, int lineNumber, int columnNumber, String received, List<String> expected) {
+
+    }
+}
