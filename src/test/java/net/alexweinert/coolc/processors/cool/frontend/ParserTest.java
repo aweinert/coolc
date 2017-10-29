b559fb313503558b39068c8b43705933ffce9664
Alexander Weinert <weinert@react.uni-saarland.de>
2017-10-29 11:04:20 +0100

    Adapt Parser-tests to DI
    We have moved the construction of the parser to dependency injection,
    so we need to adapt the test a little bit
    
    Signed-off-by: Alexander Weinert <weinert@react.uni-saarland.de>

--- src/test/java/net/alexweinert/coolc/processors/cool/frontend/ParserTest.java
+++ src/test/java/net/alexweinert/coolc/processors/cool/frontend/ParserTest.java
@@ -1,18 +1,19 @@
 package net.alexweinert.coolc.processors.cool.frontend;
 
-import java.io.FileReader;
-import java.net.URI;
-import java.nio.file.Paths;
-import java.util.Collections;
-
 import net.alexweinert.coolc.representations.cool.ast.ClassNode;
 import net.alexweinert.coolc.representations.cool.ast.Classes;
 import net.alexweinert.coolc.representations.cool.ast.Features;
 import net.alexweinert.coolc.representations.cool.ast.Program;
 import net.alexweinert.coolc.representations.cool.symboltables.IdTable;
-
 import org.junit.Assert;
 import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.io.FileReader;
+import java.net.URI;
+import java.nio.file.Paths;
+import java.util.Collections;
 
 public class ParserTest {
 
@@ -21,7 +22,11 @@ public class ParserTest {
         final URI uri = this.getClass().getResource("/parser/positive/class-a.cl").toURI();
         final String path = Paths.get(uri).toString();
 
-        final CoolParser parser = new CoolParser("class-a.cl");
+        // TODO: Actually *unit*-test parsers, i.e., leverage DI to mock some stuff
+        final ApplicationContext context = new AnnotationConfigApplicationContext(CoolParser.class);
+        final CoolParser parser = context.getBean(CoolParser.class);
+        parser.setFilename("class-a.cl");
+
         final Program actualProgram = parser.process(new FileReader(path));
         final Program expectedProgram = new Program(path, 1, new Classes(path, 1,
                 Collections.singletonList(new ClassNode(path, 1, IdTable.getInstance().addString("A"), IdTable
