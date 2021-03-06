0c79e5edbe7b9b1526e26aee33127eb53f5589bc
Alexander Weinert <alexander.weinert@gmx.net>
2021-03-06 12:37:37 +0100

    Implement API endpoints for execution and compilation and an incredibly stupid frontend

--- src/main/java/net/alexweinert/coolc/APIMain.java
+++ src/main/java/net/alexweinert/coolc/APIMain.java
@@ -1,19 +1,20 @@
 package net.alexweinert.coolc;
 
+import net.alexweinert.coolc.processors.ProcessorBuilder;
+import net.alexweinert.pipelines.ProcessorException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 
 @SpringBootApplication
 @RestController
@@ -25,21 +26,61 @@ public class APIMain {
     @Autowired
     private ResourceLoader loader;
 
-    @PostMapping("/")
+    @PostMapping(value = "/compile", produces = "application/java-archive")
     @ResponseBody
-    public ResponseEntity<Resource> compileFile(@RequestParam(name = "file") MultipartFile file) throws IOException {
-        final File tempFile = File.createTempFile("tocompile",".cool");
-        try (FileOutputStream output = new FileOutputStream(tempFile)) {
-            output.write(file.getBytes());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        final File outputFile = File.createTempFile("compiled", ".jar");
-        Main.main(new String[] { tempFile.getAbsolutePath(), "-o", outputFile.getAbsolutePath() });
+    public ResponseEntity<Resource> compileFile(@RequestParam(name = "file") MultipartFile file) throws IOException, ProcessorException {
+        final File outputFile = compileFileInternal(file);
 
         return ResponseEntity
                 .ok()
                 .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename = \"compiled.jar\"")
                 .body(loader.getResource("file:" + outputFile.getAbsolutePath()));
+    }
+
+    private File compileFileInternal(MultipartFile file) throws IOException, ProcessorException {
+        final File tempFile = File.createTempFile("tocompile",".cool");
+        final File outputFile = File.createTempFile("compiled", ".jar");
+        new ProcessorBuilder.FrontendBuilder()
+                .inputStreamToReader(file.getInputStream())
+                .fileToCool(file.getOriginalFilename())
+                .checkCool()
+                .coolToBytecode().bytecodeToJbc().jbcToFiles().filesToJar(outputFile.getName(), "CoolMain")
+                .fileToHarddrive(outputFile.getParent())
+                .compile();
+
+        return outputFile;
+    }
+
+    @PostMapping("/execute")
+    public String runProgram(@RequestParam(name = "file") MultipartFile file, @RequestParam(name = "input", defaultValue = "") final String input) throws IOException, ProcessorException {
+        final File compiledJar = compileFileInternal(file);
+
+        final String[] processOutput = new String[1];
+        final Thread runnerThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    final Process p = new ProcessBuilder("java", "-jar", compiledJar.getAbsolutePath()).start();
+                    p.getOutputStream().write(input.getBytes(StandardCharsets.UTF_8));
+                    p.getOutputStream().close();
+                    final StringBuilder stringBuilder = new StringBuilder();
+                    for(int ch; (ch = p.getInputStream().read()) != -1;) {
+                        stringBuilder.append((char)ch);
+                    }
+                    p.waitFor();
+                    processOutput[0] = stringBuilder.toString();
+                } catch (IOException | InterruptedException e) {
+                }
+            }
+        });
+        runnerThread.start();
+        try {
+            runnerThread.join(10000);
+        } catch (InterruptedException e) {
+            return ":(";
+        }
+
+        return "<html><body>" + processOutput[0] + "</body></html>";
+
     }
 }
